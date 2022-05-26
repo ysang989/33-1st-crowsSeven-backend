c6f391b362fcef5e0c2ecc6a7045035035635297
minjeong kim <ebsgreat7@gmail.com>
2022-05-26 17:44:33 +0900

    네이밍 수정

--- products/views.py
+++ products/views.py
@@ -27,15 +27,15 @@ class ProductDetailView(View):
             
             for shoe_size in range(1,len(ShoeSize.objects.all())+1):
                 for option_product in option_products.filter(shoe_size=shoe_size):
-                    count_per_shoe_size[option_product.shoe_size.size] = option_products.get(shoe_size = i).stock
+                    count_per_shoe_size[option_product.shoe_size.size] = option_products.get(shoe_size = shoe_size).stock
 
             for airpot_type in range(1,len(AirpotType.objects.all())+1):
                 for option_product in option_products.filter(airpot_type = airpot_type):
-                    count_per_airpot_type[option_product.airpot_type.name] = option_products.get(airpot_type = i).stock
+                    count_per_airpot_type[option_product.airpot_type.name] = option_products.get(airpot_type = airpot_type).stock
 
             for phone_type in range(1,len(PhoneType.objects.all())+1):
                 for option_product in option_products.filter(phone_type = phone_type):
-                    count_per_phone_type[option_product.phone_type.name] = option_products.get(phone_type = i).stock
+                    count_per_phone_type[option_product.phone_type.name] = option_products.get(phone_type = phone_type).stock
             
             if not option_products[0].shoe_size and not option_products[0].airpot_type and not option_products[0].phone_type:
                 option_information.append({
