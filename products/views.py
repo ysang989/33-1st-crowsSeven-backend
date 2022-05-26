32dff8b6a01fa47e299326d43597e95096649a6e
minjeong kim <ebsgreat7@gmail.com>
2022-05-26 17:02:28 +0900

    불필요한 라이브러리 제거

--- products/views.py
+++ products/views.py
@@ -1,8 +1,3 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.shortcuts import render
-
 from django.views       import View
 from django.http        import JsonResponse
 
