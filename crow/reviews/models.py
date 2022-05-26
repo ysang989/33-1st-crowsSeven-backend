7ff4f61ee5848de9ba8da83f3bf151fbdb45df3e
minjeong kim <ebsgreat7@gmail.com>
2022-05-26 14:14:49 +0900

    새로운 모델링

--- crow/reviews/models.py
+++ crow/reviews/models.py
@@ -3,17 +3,17 @@ from django.db    import models
 from users.models import TimeStampedModel
 
 class Review(TimeStampedModel):
-    user          = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    user           = models.ForeignKey('users.User', on_delete=models.CASCADE)
     option_product = models.ForeignKey('products.OptionProduct', on_delete=models.CASCADE)
-    title         = models.CharField(max_length=30)
-    context       = models.TextField(max_length=300)
-    password      = models.CharField(max_length=100)
-    view_count    = models.IntegerField()
+    title          = models.CharField(max_length=30)
+    context        = models.TextField(max_length=300)
+    password       = models.CharField(max_length=100)
+    view_count     = models.IntegerField()
 
     class Meta:
         db_table = "reviews"
 
-class Comment(models.Model):
+class Comment(TimeStampedModel):
     user     = models.ForeignKey('users.User', on_delete=models.CASCADE)
     review   = models.ForeignKey('Review', on_delete=models.CASCADE)
     name     = models.CharField(max_length=50)
