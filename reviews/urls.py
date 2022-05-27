dd46214e6c3b415d42c9e6ae1b75e5f46141ed5b
minjeong kim <ebsgreat7@gmail.com>
2022-05-27 14:07:02 +0900

    wip

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import ReviewView
+from .views import ReviewView, CommentView
 
 urlpatterns = [
     path("/review/<int:option_product_id>", ReviewView.as_view()),
+    path("/review/<int:option_product_id>/comment", CommentView.as_view()),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -19,72 +19,79 @@ class ReviewView(View):
             password       = data["password"]
             option_product = OptionProduct.objects.get(id = option_product_id)
 
+           
             Review.objects.create(
-                user           = user,
-                title          = title,
-                context        = context,
-                password       = password,
-                view_count     = 0,
-                option_product = option_product
+                    user           = user,
+                    title          = title,
+                    context        = context,
+                    password       = password,
+                    view_count     = 0,
+                    option_product = option_product
+                    )
+        
+
+            return JsonResponse({"message" : "SUCCESS"}, status=200)
+
+        except KeyError :
+            return JsonResponse({"message" : "KEY_ERROR"}, status=400)
+
+    
+
+
+        
+        # except Review.objects.get(option_product=option_product).Exist:
+        #     return JsonResponse({"message" : "ALREADY_EXISTS"}, status=400)
+
+
+        
+    def get(self, request, option_product_id):
+            try:
+                results = []
+                review = Review.objects.get(option_product_id=option_product_id)
+                
+                comments = {}
+                
+                for comment in Comment.objects.select_related('review').filter(review_id=review.id):
+                    comments[comment.name]= comment.content
+                                    
+                if Review.objects.filter(option_product_id=option_product_id).exists():
+                    review.view_count = review.view_count+1
+                    review.save()
+
+                results.append({
+                    '제목' : review.title,
+                    '작성일': review.created_at,
+                    '조회수': review.view_count,
+                    '댓글' : comments,
+                })
+
+                return JsonResponse({"message" : results}, status=200)
+
+            except KeyError :
+                return JsonResponse({"message" : "KEY_ERROR"}, status=400)
+
+class CommentView(View):
+    @jwt_expression
+    def post(self, request, option_product_id):
+        try:
+            data = json.loads(request.body)
+
+            user         = User.objects.get(id=request.user)
+            review       = Review.objects.get(option_product_id=option_product_id)
+            name         = data["name"]
+            content      = data["content"]
+            password     = data["password"]
+
+            Comment.objects.create(
+                user     = user,
+                review    = review,
+                name     = name,
+                content  = content,
+                password = password
             )
 
             return JsonResponse({"message" : "SUCCESS"}, status=200)
 
         except KeyError :
             return JsonResponse({"message" : "KEY_ERROR"}, status=400)
-        
-        except option_product.exists():
-            return JsonResponse({"message" : "ALREADY_EXISTS"}, status=400)
-
-
-        
-#     def get(self, request, id):
-#             try:
-#                 results = []
-
-#                 comments = {}
-#                 for comment in Comment.objects.select_related('review').filter(review=id):
-#                     comments[comment.name]= comment.content                
-#                 # [comments[comment.datacomment.context) for comment in Comment.objects.select_related('review').filter(review=id)]                    
-#                 review = Review.objects.get(id=id)
-#                 if Review.objects.filter(id=id).exists():
-#                     review.view_count = review.view_count+1
-#                     review.save()
-
-#                 results.append({
-#                     '제목' : review.title,
-#                     '작성일': review.created_at,
-#                     '조회수': review.view_count,
-#                     '댓글' : comments,
-#                 })
-
-#                 return JsonResponse({"message" : results}, status=200)
-
-#             except KeyError :
-#                 return JsonResponse({"message" : "KEY_ERROR"}, status=400)
-
-# class CommentView(View):
-#     @jwt_expression
-#     def post(self, request, id):
-#         try:
-#             data = json.loads(request.body)
-
-#             user         = request.user
-#             review       = Review.objects.get(id=id).id
-#             name         = data["name"]
-#             content      = data["content"]
-#             password     = data["password"]
-
-#             Comment.objects.create(
-#                 user     = user,
-#                 reiew    = review,
-#                 name     = name,
-#                 content  = content,
-#                 password = password
-#             )
-
-#             return JsonResponse({"message" : "SUCCESS"}, status=200)
-
-#         except KeyError :
-#             return JsonResponse({"message" : "KEY_ERROR"}, status=400)
         
