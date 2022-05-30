86262a3bdcaf891a85d2ace872cc17055a20ca62
minjeong kim <ebsgreat7@gmail.com>
2022-05-30 15:20:09 +0900

    리뷰작성api

--- reviews/urls.py
+++ reviews/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .views      import ReviewView
 
 urlpatterns = [
-    path("/write", ReviewView.as_view()),
+    path("/", ReviewView.as_view()),
 ]
--- utils.py
+++ utils.py
@@ -9,9 +9,9 @@ from users.models   import User
 def login_decorator(func):
     def wrapper(self, request, *args, **kwargs):
         try:
-            access_token = request.headers.get("Authrozation", None)
+            access_token = request.headers.get("Authorization", None)
             payload      = jwt.decode(access_token, settings.SECRET_KEY, algorithms = settings.ALGORITHM)
-            user         = User.objects.get(id = payload["id"])
+            user         = User.objects.get(id = payload["id"]).id
             request.user = user
 
         except jwt.exceptions.DecodeError:
