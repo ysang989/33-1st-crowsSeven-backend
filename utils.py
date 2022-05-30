f95f133af6c0d5db04ef2bd0e76461b0355ef784
minjeong kim <ebsgreat7@gmail.com>
2022-05-30 14:00:01 +0900

    api

--- utils.py
+++ utils.py
@@ -10,7 +10,7 @@ def login_decorator(func):
     def wrapper(self, request, *args, **kwargs):
         try:
             access_token = request.headers.get("Authrozation", None)
-            payload      = jwt.decode(access_token, SECRET_KEY, algorithms = "HS256")
+            payload      = jwt.decode(access_token, settings.SECRET_KEY, algorithms = settings.ALGORITHM)
             user         = User.objects.get(id = payload["id"])
             request.user = user
 
