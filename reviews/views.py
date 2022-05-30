203e6dfbdba24fc8de3c234e15c3980ad7984bd6
minjeong kim <ebsgreat7@gmail.com>
2022-05-30 21:06:06 +0900

    댓글작성기능

--- reviews/views.py
+++ reviews/views.py
@@ -1,15 +1,9 @@
 import json
-import datetime
 
 from django.views       import View
 from django.http        import JsonResponse
-from django.db.models   import Q
-from django.shortcuts   import redirect
 
 from reviews.models     import Review, Comment
-from products.models    import Product
-from orders.models      import Order, OrderItem
-from products.models    import OptionProduct, Product
 from users.models       import User
 from utils              import login_decorator
 
@@ -18,6 +12,7 @@ class CommentView(View):
     def post(self, request, review_id):
         try:
             data = json.loads(request.body)
+            
             user         = User.objects.get(id=request.user)
             review       = Review.objects.get(id=review_id)
             name         = data["name"]
