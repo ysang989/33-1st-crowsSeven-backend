8faa61c64f108eb6c840a9e4d77c10ec92ca2823
minjeong kim <ebsgreat7@gmail.com>
2022-05-30 20:07:37 +0900

    리뷰삭제기능

--- reviews/views.py
+++ reviews/views.py
@@ -5,11 +5,7 @@ from django.views       import View
 from django.http        import JsonResponse
 from django.db.models   import Q
 
-from reviews.models     import Review, Comment
-from products.models    import Product
-from orders.models      import Order, OrderItem
-from products.models    import OptionProduct, Product
-from users.models       import User
+from reviews.models     import Review
 from utils              import login_decorator
 from django.shortcuts   import redirect
 
@@ -20,7 +16,7 @@ class ReviewView(View):
             review = Review.objects.get(id=review_id)
             review.delete()
 
-            return redirect('/')
+            return redirect('/reviews')
             
         except KeyError :
             return JsonResponse({"message" : "KEY_ERROR"}, status=400)
