68a82dfe1ac0ef9ba9cef6a0575c77d65ee10730
minjeong kim <ebsgreat7@gmail.com>
2022-05-31 01:19:24 +0900

    사용자가 리뷰할 수 있는 아이템 목록 전달 기능

--- reviews/views.py
+++ reviews/views.py
@@ -11,27 +11,29 @@ class ReviewView(View):
     @login_decorator
     def get(self, request):
         try:
-            total_option_product    = []
-            selected_products_name  = []
-            selected_products       = []
-            total_option            = []
+            total_option_product     = []
+            selected_products_names  = []
+            selected_products        = []
+            total_option             = []
 
-            total_order=Order.objects.filter(user_id=request.user).select_related('user').prefetch_related('orderitem_set')
+            total_order=Order.objects.filter(user_id=request.user).select_related('user').\
+                prefetch_related('orderitem_set')
 
             for total_option_product in total_order:
                 total_option.append(list(total_option_product.orderitem_set.values('option_product_id')))
 
             total_option_product_names = sum(total_option,[])
             
-            [selected_products.append(total_option_product_names[i]['option_product_id']) for i in range(len(total_option_product_names))]
+            [selected_products.append(total_option_product_names[i]['option_product_id'])\
+                for i in range(len(total_option_product_names))]
 
-            selected_products_name = [{
+            selected_products_names = [{
                 "제품이름"  : OptionProduct.objects.get(id=selected_product).product.name,
                 "제품아이디" : OptionProduct.objects.get(id=selected_product).product.id
             } for selected_product in selected_products ]
            
-            selected_products_name = list({v['제품이름']:v for v in selected_products_name}.values())
-            return JsonResponse({"message" : selected_products_name}, status=200)
+            selected_products_names = list({selected_products_name['제품이름']:selected_products_name for selected_products_name in selected_products_names}.values())
+            return JsonResponse({"message" : selected_products_names}, status=200)
             
         except KeyError :
             return JsonResponse({"message" : "KEY_ERROR"}, status=400)
