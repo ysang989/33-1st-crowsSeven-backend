6baf011b23838b19b761d28b9ecf448598da95ff
minjeong kim <ebsgreat7@gmail.com>
2022-05-31 04:42:57 +0900

    카트삭제기능

--- carts/views.py
+++ carts/views.py
@@ -1,6 +1,6 @@
-from django.views import View
 import datetime
 
+from django.views    import View
 from django.http     import JsonResponse
 
 from users.models    import User
@@ -9,12 +9,22 @@ from carts.models    import Cart
 from utils           import login_decorator
 
 class CartView(View):
+    @login_decorator
     def delete(self, request, cart_id):
         try:
             product = Cart.objects.get(id = cart_id)
             product.delete()
 
-            return JsonResponse({'results' : "DELETE_SUCCESS"}, status=200)
+            cart_products = Cart.objects.filter(user_id = request.user)
+
+            results=[{
+                "product_image": cart_product.option_product.product.thumbnail_image_url,
+                "product_name" : cart_product.option_product.product.name,
+                "product_count": cart_product.count,
+                "product_price": cart_product.option_product.product.price,
+            }for cart_product in cart_products]
+
+            return JsonResponse({'results' : results}, status=200)
 
         except KeyError :
             return JsonResponse({"message" : "KEY_ERROR"}, status=400)
