144380c2a0efec5398f68008ca675aa241db487e
minjeong kim <ebsgreat7@gmail.com>
2022-05-31 04:13:12 +0900

    카트물품등록기능

--- carts/views.py
+++ carts/views.py
@@ -1,55 +1,49 @@
-from django.views import View
 import datetime
 
+from django.views    import View
 from django.http     import JsonResponse
 
 from products.models import OptionProduct
 from carts.models    import Cart
-from users.models  import User
-from utils         import login_decorator
+from users.models    import User
+from utils           import login_decorator
 
 class CartView(View):
     @login_decorator
     def get(self, request):
         try:
-            carts = Cart.objects.filter(user_id = request.user)
-            for cart in carts:
-                if cart.created_at >= datetime.datetime.now() +datetime.timedelta(minutes=2):
-                    cart.delete()
+            carts = Cart.objects.filter(created_at__lt = datetime.datetime.now() - datetime.timedelta(hours=24))
+            if carts:
+                [cart.delete() for cart in carts]
+
             count               = request.GET.get('count', None)
             selected_product_id = request.GET.get('option_product_id', None)
+            option_name         = request.GET.get('option_name', None)
 
-            if product_id:
-                selected_product = OptionProduct.objects.prefetch_related('cart_set').get(id=selected_product_id)
+            cart_products = Cart.objects.select_related('option_product').filter(user_id = request.user)
+            carts = cart_products.filter(option_product = selected_product_id)
 
-                if selected_product in carts:
-                    selected_product.count += int(count)
-                    selected_product.save()
+            if selected_product_id:
+                if carts.exists():
+                    cart = carts.get(option_product_id = OptionProduct.objects.get(id=selected_product_id))
+                    cart.count += int(count)
+                    cart.save()
 
                 else:
-                    Cart.objects.update_or_create(
+                    Cart.objects.create(
                         user           = User.objects.get(id=request.user),
-                        option_product = selected_product,
+                        option_product = OptionProduct.objects.get(id=selected_product_id),
                         count          = count
                     )
-
-                results = [{
-                    "product_thumbnail_image_url": cart.option_product.product.thumbnail_image_url,
-                    "product_name"               : cart.option_product.product.name,
-                    "option_name"                : option_name,
-                    "product_price"              : cart.option_product.product.price,
-                    "product_count"              : cart.count
-                }for cart in carts]
-
-            else:
-                results = [{
-                    "product_thumbnail_image_url": cart.option_product.product.thumbnail_image_url,
-                    "product_name"               : cart.option_product.product.name,
-                    "option_name"                : option_name,
-                    "product_price"              : cart.option_product.product.price,
-                    "product_count"              : cart.count,
-                    "cart_id"                    : cart.id
-                }for cart in carts]
+    
+            results = [{
+                "product_thumbnail_image_url": cart.option_product.product.thumbnail_image_url,
+                "product_name"               : cart.option_product.product.name,
+                "option_name"                : option_name,
+                "product_price"              : cart.option_product.product.price,
+                "product_count"              : cart.count,
+                "cart_id"                    : cart.id
+            } for cart in cart_products]
 
             return JsonResponse({'results' : results}, status=200)
 
