642c2282997e9665823a99872f1e92624e6e46e0
minjeong kim <ebsgreat7@gmail.com>
2022-05-31 02:01:32 +0900

    장바구니추가기능

--- carts/views.py
+++ carts/views.py
@@ -12,31 +12,26 @@ class CartView(View):
     @login_decorator
     def get(self, request):
         try:
-            product_id          = request.GET.get('product_id', None)
-            option_name         = request.GET.get('option_name', None)
+            carts = Cart.objects.filter(user_id = request.user)
+            for cart in carts:
+                if cart.created_at >= datetime.datetime.now() +datetime.timedelta(minutes=2):
+                    cart.delete()
             count               = request.GET.get('count', None)
             selected_product_id = request.GET.get('option_product_id', None)
 
             if product_id:
-                selected_product = OptionProduct.objects.get(id=selected_product_id)
+                selected_product = OptionProduct.objects.prefetch_related('cart_set').get(id=selected_product_id)
 
-                if Cart.objects.filter(option_product = selected_product_id).exists():
-                    cart = Cart.objects.get(option_product = selected_product_id)
-                    cart.count += int(count)
-                    cart.save()
+                if selected_product in carts:
+                    selected_product.count += int(count)
+                    selected_product.save()
 
                 else:
                     Cart.objects.update_or_create(
                         user           = User.objects.get(id=request.user),
-                        option_product = OptionProduct.objects.get(id=selected_product.id),
+                        option_product = selected_product,
                         count          = count
                     )
-
-                carts = Cart.objects.filter(user_id = request.user)
-
-                for cart in carts:
-                    if cart.created_at == datetime.datetime.now() -datetime.timedelta(minutes=10):
-                        cart.delete()
 
                 results = [{
                     "product_thumbnail_image_url": cart.option_product.product.thumbnail_image_url,
@@ -47,11 +42,6 @@ class CartView(View):
                 }for cart in carts]
 
             else:
-                carts = Cart.objects.filter(user_id = request.user)
-                for cart in carts:
-                    if cart.created_at == datetime.datetime.now() -datetime.timedelta(minutes=10):
-                        cart.delete()
-
                 results = [{
                     "product_thumbnail_image_url": cart.option_product.product.thumbnail_image_url,
                     "product_name"               : cart.option_product.product.name,
