69fb0c34bd62e5ae76c1b5d2dc52873203154c3f
minjeong kim <ebsgreat7@gmail.com>
2022-05-30 17:14:59 +0900

    장바구니 삭제 기능

--- carts/views.py
+++ carts/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.views import View
+import datetime
 
-# Create your views here.
+from django.http     import JsonResponse
+
+from users.models    import User
+from products.models import OptionProduct
+from carts.models    import Cart
+from utils           import login_decorator
+
+class CartView(View):
+    def delete(self, request, cart_id):
+        try:
+            product = Cart.objects.get(id = cart_id)
+            product.delete()
+
+            return JsonResponse({'results' : "DELETE_SUCCESS"}, status=200)
+
+        except KeyError :
+            return JsonResponse({"message" : "KEY_ERROR"}, status=400)
