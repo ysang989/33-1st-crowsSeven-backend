f6c35b197d9b25ff8ac0b8d4e18a1824246875af
minjeong kim <ebsgreat7@gmail.com>
2022-05-30 17:17:59 +0900

    카트에 있는 물품 수량 업데이트 기능

--- carts/views.py
+++ carts/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.views import View
+import datetime
 
-# Create your views here.
+from django.http     import JsonResponse
+
+from products.models import OptionProduct
+from carts.models    import Cart
+from users.models  import User
+from utils         import login_decorator
+
+class CartView(View):
+    @login_decorator
+    def patch(self, request, cart_id):
+        try:
+            option_product_count = request.GET.get('qty')
+            product              = Cart.objects.get(id = cart_id)
+            
+            product.count = option_product_count
+            product.save()
+
+            return JsonResponse({'results' : "success"}, status=200)
+
+        except KeyError :
+            return JsonResponse({"message" : "KEY_ERROR"}, status=400)
+    
