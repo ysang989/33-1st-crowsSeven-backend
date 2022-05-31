e0579399187eb7f2c1fcb804ec44c3e185d3f1e3
minjeong kim <ebsgreat7@gmail.com>
2022-05-31 18:32:19 +0900

    리뷰작성기능

--- reviews/urls.py
+++ reviews/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .views      import ReviewView
 
 urlpatterns = [
-    path("/", ReviewView.as_view()),
+    path("/product/<int:product_id>", ReviewView.as_view()),
 ]
