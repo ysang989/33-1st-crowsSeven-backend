0076d8424f62720f32674e3c0f8c865e16cdc9d2
HoJun Jang <wkdghwns91@gmail.com>
2022-05-31 16:22:52 +0900

    feature/review-write

--- reviews/urls.py
+++ reviews/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from .views      import ReviewView
 
 urlpatterns = [
-    path("/", ReviewView.as_view()),
+    # :8000/reviews/product/1
+    path("/product/<int:product_id>", ReviewView.as_view()),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -12,25 +12,23 @@ from utils              import login_decorator
 
 class ReviewView(View):
     @login_decorator
-    def post(self, request):
+    def post(self, request, product_id):
         try:
             data       = json.loads(request.body)
-            user       = request.user
             title      = data["title"]
             context    = data["context"]
             password   = data["password"]
-            product    = data["product_id"]
         
-            if Review.objects.filter(product_id=product , user_id = user.id).exists():
+            if Review.objects.filter(product_id=product_id , user = request.user).exists():
                 return JsonResponse({'message':'REVIEW_ALREADY_EXIST'}, status=404)
             
             Review.objects.create(
-                user       = user,
+                user       = request.user,
                 title      = title,
                 context    = context,
                 password   = password,
                 view_count = 0,
-                product    = Product.objects.get(id=product),
+                product    = Product.objects.get(id=product_id),
             )
 
             return JsonResponse({"message" : "SUCCESS"}, status=201)
