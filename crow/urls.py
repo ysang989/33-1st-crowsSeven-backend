f45abfe1ae2669edfc55d280c0063d7e136cd827
minjeong kim <ebsgreat7@gmail.com>
2022-05-31 18:42:59 +0900

    리뷰삭제기능

--- crow/urls.py
+++ crow/urls.py
@@ -3,9 +3,5 @@ from django.urls import path, include
 urlpatterns = [
     path("users", include("users.urls")),
     path('products', include('products.urls')),
-<<<<<<< HEAD
-    path('reviews', include('reviews.urls')),
-=======
     path('reviews', include('reviews.urls'))
->>>>>>> main
 ]
