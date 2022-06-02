5371ef91049df581a7cbad1c21c95e084dc606ee
minjeong kim <ebsgreat7@gmail.com>
2022-06-02 17:11:02 +0900

    feature/delete

--- carts/views.py
+++ carts/views.py
@@ -12,19 +12,10 @@ class CartView(View):
     @login_decorator
     def delete(self, request, cart_id):
         try:
-            product = Cart.objects.get(id = cart_id)
-            product.delete()
+            cart = Cart.objects.get(id = cart_id)
+            cart.delete()
 
-            cart_products = Cart.objects.filter(user_id = request.user)
-
-            results=[{
-                "product_image": cart_product.option_product.product.thumbnail_image_url,
-                "product_name" : cart_product.option_product.product.name,
-                "product_count": cart_product.count,
-                "product_price": cart_product.option_product.product.price,
-            }for cart_product in cart_products]
-
-            return JsonResponse({'results' : results}, status=201)
+            return JsonResponse({'message' : "DELETE_SUCCESS"}, status=201)
 
         except Cart.DoesNotExist:
             return JsonResponse({"message" : "CART_NOT_EXISTED"}, status=400)
